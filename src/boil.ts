c3c74d9c4e0c9d921b635a272a9626603c29b68a
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2021-06-06 23:22:25 -0400

    Refactor to a thing of TypeScript beauty.

--- src/boil.ts
+++ src/boil.ts
@@ -1,136 +1,188 @@
-import { readFileSync } from "fs";
-import { parse } from "path";
 import "reflect-metadata";
 
+import { readFileSync } from "fs";
+import { parse } from "path";
 import Handlebars from "handlebars";
 import figlet from "figlet";
 import { ERSchema, loadSchema } from "./er-schema";
 import pluralize from "pluralize";
 import { Command } from "commander";
 import { sync } from "walkdir/walkdir";
+import Debug from "debug";
+
+const debug = Debug("boil");
 
 type TemplateFunctionMap = Map<string, HandlebarsTemplateDelegate>;
+
+class Banner {
+  private readonly visible: boolean;
+
+  constructor(visible = true) {
+    this.visible = visible;
+  }
+
+  show(text: string) {
+    if (this.visible) {
+      console.log(figlet.textSync(text));
+    }
+  }
+}
+
+class TemplateEngine {
+  private readonly templateMap: TemplateFunctionMap;
+
+  constructor(
+    private readonly verbose: boolean,
+    private readonly banner: Banner
+  ) {
+    this.templateMap = new Map();
+    this.registerHelpers();
+    this.loadTemplates();
+  }
+
+  private registerHelpers() {
+    Handlebars.registerHelper("lower", (str: string) => str.toLowerCase());
+    Handlebars.registerHelper("plural", (str: string) => pluralize(str));
+  }
+
+  private loadTemplates() {
+    for (const path of sync("templates")) {
+      if (path.endsWith(".hbs")) {
+        const pathInfo = parse(path);
+        const key = pathInfo.name.replace(/\..*/, "");
+        const template = readFileSync(path, "utf-8");
+        const templateFunction = Handlebars.compile(template, {
+          noEscape: true,
+        });
+        this.templateMap.set(key, templateFunction);
+      }
+    }
+
+    if (this.verbose) {
+      this.banner.show("templates");
+      this.templateMap.forEach((value, key) => console.log(key));
+    }
+  }
+
+  render(templateKey: string, context: any) {
+    const templateFunction = this.templateMap.get(templateKey);
+    if (templateFunction) {
+      return templateFunction(context);
+    } else {
+      throw Error(`No template for key '${templateKey}'`);
+    }
+  }
+}
+
+class Boiler {
+  private readonly schema: ERSchema;
+
+  constructor(
+    schemaName: string,
+    private readonly engine: TemplateEngine,
+    private readonly banner: Banner
+  ) {
+    this.schema = loadSchema(schemaName);
+  }
+
+  boil(options: { [key: string]: string }) {
+    const jumpTable: [string, Function][] = [
+      ["verbose", this.showDetails],
+      ["entity", this.generateEntity],
+      ["module", this.generateModule],
+      ["service", this.generateService],
+      ["resolver", this.generateResolver],
+      ["graphql", this.generateGraphQL],
+      ["table", this.generateTable],
+      ["createUpdate", this.generateCreateUpdate],
+    ];
+    for (let [opt, func] of jumpTable) {
+      if (options[opt] || options.all) {
+        func.call(this);
+      }
+    }
+  }
+
+  generateEntity() {
+    this.banner.show("entity");
+    console.log(
+      this.engine.render("entity", {
+        entity: this.schema.entity,
+        ...this.schema.declareFields(),
+      })
+    );
+  }
+
+  showDetails() {
+    this.banner.show(this.schema.inflections.entityLower);
+    console.log(JSON.stringify(this.schema, null, 2));
+    this.banner.show("inflections");
+    console.log(JSON.stringify(this.schema.inflections, null, 2));
+  }
+
+  generateModule() {
+    this.banner.show("module");
+    console.log(
+      this.engine.render("module", {
+        entityName: this.schema.inflections.entityUpper,
+      })
+    );
+  }
+
+  generateResolver() {
+    this.banner.show("resolver");
+    console.log(
+      this.engine.render("resolver", {
+        entityName: this.schema.inflections.entityUpper,
+        entityNamePlural: this.schema.inflections.entityUpperPlural,
+      })
+    );
+  }
+
+  generateService() {
+    this.banner.show("service");
+    console.log(
+      this.engine.render("service", {
+        entityName: this.schema.inflections.entityUpper,
+        entityNamePlural: this.schema.inflections.entityUpperPlural,
+      })
+    );
+  }
+
+  generateTable() {
+    this.banner.show("table");
+    console.log(this.engine.render("table", this.schema.inflections));
+  }
+
+  generateCreateUpdate() {
+    this.banner.show("create-update");
+    console.log(this.engine.render("create-update", this.schema.inflections));
+  }
+
+  generateGraphQL() {
+    this.banner.show("graphql");
+    console.log(this.engine.render("crud", this.schema.inflections));
+  }
+}
 
 const program = new Command();
 
 program
-  .usage("[options] schemaFileName")
+  .arguments("<schema-file>")
   .option("-e --entity", "generate entity")
   .option("-m --module", "generate module")
   .option("-r --resolver", "generate resolver")
   .option("-s --service", "generate service")
+  .option("-t --table", "generate Vue table")
+  .option("-c --create-update", "generate Vue create-update")
   .option("-a --all", "generate all")
-  .option("-v --verbose", "be verbose");
+  .option("-v --verbose", "be verbose")
+  .action((schemaFile: string, options) => {
+    debug("schemaFile %O, options %O", schemaFile, options);
+    const banner = new Banner();
+    const engine = new TemplateEngine(program.opts().verbose, banner);
+    const boiler = new Boiler(schemaFile, engine, banner);
+    boiler.boil(options);
+  });
 
 program.parse();
-
-const templateMap: TemplateFunctionMap = new Map();
-const options = program.opts();
-if (options.verbose) {
-  console.log("OPTIONS", options);
-}
-
-if (program.args.length === 1) {
-  main(program.args[0]);
-} else {
-  program.help();
-}
-
-function registerHelpers() {
-  Handlebars.registerHelper("lower", (str: string) => str.toLowerCase());
-  Handlebars.registerHelper("plural", (str: string) => pluralize(str));
-}
-
-function banner(text: string) {
-  console.log(figlet.textSync(text));
-}
-
-function loadTemplates() {
-  for (const path of sync("templates")) {
-    if (path.endsWith(".hbs")) {
-      const pathInfo = parse(path);
-      const key = pathInfo.name.replace(/\..*/, "");
-      const template = readFileSync(path, "utf-8");
-      const templateFunction = Handlebars.compile(template, {
-        noEscape: true,
-      });
-      templateMap.set(key, templateFunction);
-    }
-  }
-
-  if (options.verbose) {
-    banner("templates");
-    templateMap.forEach((value, key) => console.log(key));
-  }
-}
-
-function renderTemplate(templateKey: string, context: object) {
-  const templateFunction = templateMap.get(templateKey);
-  if (templateFunction) {
-    return templateFunction(context);
-  } else {
-    throw Error(`No template for key '${templateKey}'`);
-  }
-}
-
-function generateEntity(schema: ERSchema) {
-  return renderTemplate("entity", {
-    entity: schema.entity,
-    ...schema.declareFields(),
-  });
-}
-
-function main(schemaName: string) {
-  registerHelpers();
-  loadTemplates();
-
-  const schema = loadSchema(schemaName);
-
-  if (options.verbose) {
-    banner(schema.inflections.entityLower);
-    console.log(JSON.stringify(schema, null, 2));
-    banner("inflections");
-    console.log(JSON.stringify(schema.inflections, null, 2));
-  }
-
-  if (options.entity || options.all) {
-    banner("entity");
-    console.log(generateEntity(schema));
-  }
-
-  if (options.module || options.all) {
-    banner("module");
-    console.log(
-      renderTemplate("module", { entityName: schema.inflections.entityUpper })
-    );
-  }
-
-  if (options.resolver || options.all) {
-    banner("resolver");
-    console.log(
-      renderTemplate("resolver", {
-        entityName: schema.inflections.entityUpper,
-        entityNamePlural: schema.inflections.entityUpperPlural,
-      })
-    );
-  }
-
-  if (options.service || options.all) {
-    banner("service");
-    console.log(
-      renderTemplate("service", {
-        entityName: schema.inflections.entityUpper,
-        entityNamePlural: schema.inflections.entityUpperPlural,
-      })
-    );
-  }
-
-  banner("table");
-  console.log(renderTemplate("table", schema.inflections));
-
-  banner("create-update");
-  console.log(renderTemplate("create-update", schema.inflections));
-
-  banner("graphql");
-  console.log(renderTemplate("crud", schema.inflections));
-}
